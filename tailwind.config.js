db600fc442aa42a46839014084b2da1c9ab6f196
s-ja <dkstmdwl0615@naver.com>
2022-12-14 20:52:49 +0900

    tailwind.css config color error fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,15 @@ module.exports = {
       fontFamily: {
         sans: ["pretendard", "Arial", "sans-serif"], 
         // sans가 제일 기본 상속 폰트이므로 전체 폰트바꾸려면 sans재지정후 맨앞에 원하는 폰트 넣기
-      }
-    },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'black': '#000000',
-      'blue': '#4C5FD5',
-      'lilac': '#dadbf1',
+      },
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'white': '#ffffff',
+        'black': '#000000',
+        'blue': '#4C5FD5',
+        'lilac': '#dadbf1',
+      },
     },
   },
   plugins: [],
